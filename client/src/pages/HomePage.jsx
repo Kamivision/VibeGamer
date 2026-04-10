89f8d3a7b26e6f59e990d4a832d87819c3f17b39
kamivision <kamivision@gmail.com>
2026-04-10 16:07:35 -0500

    started setting basic frontend routes

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function HomePage() {
+    return (
+        <div className="home-page">
+            <h1>Welcome to VibeGamer!</h1>
+            <p>Discover and share your favorite games with the community.</p>
+        </div>
+    );
+}
